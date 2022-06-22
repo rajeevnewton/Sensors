77b2322718110ae20d43dd3c012c5c83622a1550
rajeevnewton <101967606+rajeevnewton@users.noreply.github.com>
2022-06-22 09:40:01 +0530

    Add files via upload

---
+++ ultra_servo.ino
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////
+#include <Servo.h>
+
+Servo servo1;  // create servo object to control a servo
+
+
+///////////////////////////////////////////////////////
+int inches = 0;
+
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  servo1.attach(9); 
+}
+
+void loop()
+{
+  /////////////////////////////////////////////////////////////////////
+  Serial.println("servo to 0 degrees: ");      
+  servo1.write(0); //0 left
+
+  delay(1000);  
+  
+  Serial.println("servo to 90 degrees: ");   
+  servo1.write(0); // 90 front
+  
+  delay(1000);
+   
+  Serial.println("servo to 180 degrees: ");   
+  servo1.write(0); //180 left
+  
+  delay(1000);  
+  //////////////////////////////////////////////////////////////
+  // measure the ping time in cm
+  
+  cm = 0.01723 * readUltrasonicDistance(7, 7);
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+  
+  Serial.print(cm);
+  Serial.println("cm");
+  delay(100); // Wait for 100 millisecond(s)
+  
+}
